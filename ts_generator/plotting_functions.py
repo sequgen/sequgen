dec22d71783258b6de5dc0ccbffd43734e4a826c
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-12 14:08:27 +0200

    move functions to folder and do some linting

--- ts_generator/plotting_functions.py
+++ ts_generator/plotting_functions.py
@@ -1,20 +1,40 @@
 import numpy as np
 from sklearn import metrics
-#from sklearn.utils.multiclass import unique_labels
+import matplotlib
 from matplotlib import pyplot as plt
+
+
+def plot_TS(X_ts: np.ndarray, TS_def: dict):
+    """ Plot multivariate time series.
+    """
+    n_ch = X_ts.shape[0]
+
+    cmap = matplotlib.cm.get_cmap('inferno') #'Spectral')
+    plt.style.use('ggplot')
+    plt.figure(figsize=(10, (1 + 0.7 *n_ch)))
+    for i in range(n_ch):
+        plt.plot((X_ts[i, :] - i), color=cmap(i / n_ch))
+
+    if 'class_name' in TS_def:
+        title = TS_def['class_name']
+    else:
+        title = 'Generated time series'
+    plt.title(title)
+    plt.yticks(-np.arange(n_ch), ['channel ' + str(i) for i in range(n_ch)])
+    plt.xlabel('time')
 
 
 def plot_confusion_matrix(y_true, y_pred, classes,
                           normalize=False,
                           title=None,
                           cmap=plt.cm.Blues,
-                          figsize= (8,8)):
+                          figsize=(8, 8)):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
     plt.style.use('ggplot')
-    
+
     if not title:
         if normalize:
             title = 'Normalized confusion matrix'
@@ -31,13 +51,11 @@ def plot_confusion_matrix(y_true, y_pred, classes,
     else:
         print('Confusion matrix, without normalization')
 
-    #print(cm)
-    
     fig, ax = plt.subplots(figsize=figsize)
     im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
-    ax.grid(False) 
-    
+    ax.grid(False)
+
     # Show all ticks...
     ax.set(xticks=np.arange(cm.shape[1]),
            yticks=np.arange(cm.shape[0]),
@@ -60,5 +78,5 @@ def plot_confusion_matrix(y_true, y_pred, classes,
                     ha="center", va="center",
                     color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
-    
-    return cm, #ax
+
+    return cm
