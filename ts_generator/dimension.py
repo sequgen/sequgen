1969d7b701a3543237122b1c1f999aa0dcf0b156
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-18 12:14:02 +0100

    parameter space dimension can now be collapsed

--- ts_generator/dimension.py
+++ ts_generator/dimension.py
@@ -1,5 +1,8 @@
 class Dimension():
-    def __init__(self, name, lower_bound, upper_bound):
+    def __init__(self, name, lower_bound, upper_bound=None):
         self.name = name
         self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+        if upper_bound is None:
+            self.upper_bound = lower_bound
+        else:
+            self.upper_bound = upper_bound
