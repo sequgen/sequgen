2ea0cfe6b0042ec3347a8b1f92332d21624d42df
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-18 12:13:03 +0100

    renamed white -> gaussian

---
+++ ts_generator/stochastic/gaussian.py
@@ -0,0 +1,17 @@
+import numpy
+
+
+def gaussian(t_predict, stddev=1, average=0, correlation_length=0):
+    n_elems = len(t_predict)
+    if correlation_length == 0:
+        return average + stddev * numpy.random.randn(n_elems)
+    else:
+        draws = numpy.random.randn(n_elems)
+        correlated = numpy.full(n_elems, numpy.nan)
+        for i, t in enumerate(t_predict):
+            if i == 0:
+                correlated[i] = draws[i]
+            else:
+                corr = max(0, (t_predict[i - 1] - t_predict[i] + correlation_length) / correlation_length)
+                correlated[i] = correlated[i - 1] * corr + draws[i] * (1 - corr)
+        return average + stddev * correlated
--- ts_generator/stochastic/white.py
+++
@@ -1,6 +0,0 @@
-import numpy
-
-
-def white(t_predict, stddev=1, average=0):
-    n_elems = len(t_predict)
-    return average + stddev * numpy.random.randn(n_elems)
