df8420b19c2f0a594cf78ae3a22deefcb732a338
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-03-23 16:01:36 +0100

    renamed folder

--- ts_generator/deterministic/constant.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-
-def constant(t_predict, value):
-    return numpy.ones_like(t_predict) * value
--- ts_generator/deterministic/sine.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-
-def sine(t_predict, phase_shift=0, amplitude=1.0, wavelength=1.0, average=0.0):
-    return average + amplitude * numpy.sin(2 * numpy.pi * (t_predict - phase_shift) / wavelength)
