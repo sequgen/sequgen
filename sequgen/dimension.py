00e775b813e4193042516b9001c9c2397721c639
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-30 14:56:45 +0200

    added some apidoc strings

--- sequgen/dimension.py
+++ sequgen/dimension.py
@@ -3,11 +3,22 @@ from typing import Union
 
 # pylint: disable=too-few-public-methods
 class Dimension:
-    """ """
+    """Class representing one dimension of a parameter space."""
     Bound = Union[float, int]
 
     def __init__(self, name: str, lower_bound: Bound, upper_bound: Bound = None):
-        """ """
+        """Constructor
+
+        Constructor.
+
+        Args:
+          name (str):
+            Name of the dimension.
+          lower_bound (Bound):
+            Lower bound of the dimension.
+          upper_bound (Bound):
+            Upper bound of the dimension.
+        """
         self.name = name
         self.lower_bound = lower_bound
         if upper_bound is None:
--- sequgen/parameter_space.py
+++ sequgen/parameter_space.py
@@ -6,12 +6,19 @@ from sequgen.samplers.sample_uniform_random import sample_uniform_random
 
 class ParameterSpace:
 
-    """ """
+    """Class representing a parameter space."""
 
     Dimensions = Iterable[Dimension]
 
     def __init__(self, dimensions: Dimensions, sampler: Callable = None):
-        """ """
+        """Constructor
+
+        Constructor.
+
+        Args:
+          dimensions (Dimensions): dimensions of the parameter space.
+          sampler (Callable): callable with which to draw a sample from the parameter space
+          """
         self.dimension_names = list()
         self.lower_bounds = list()
         self.upper_bounds = list()
@@ -28,17 +35,17 @@ class ParameterSpace:
         self.ndims = len(self.dimension_names)
 
     def __repr__(self):
-        """ """
+        """String representation of the parameter space."""
         dimensions_string = ", ".join(["'{0}'".format(name) for name in self.dimension_names])
         return "{0}-D parameter space with dimensions: {1}".format(self.ndims, dimensions_string)
 
     def format_str(self):
-        """ """
+        """Format string that can be used to print formatted information about the dimensions of the parameter space"""
         names = ["{0}={1}{0}:.2f{2}".format(name, "{", "}") for name in self.dimension_names]
         return ", ".join(names)
 
     def sample(self):
-        """ """
+        """Draw a sample from the parameter space. Defaults to uniform random draw."""
         return self.sampler(dimension_names=self.dimension_names,
                             lower_bounds=self.lower_bounds,
                             upper_bounds=self.upper_bounds)
