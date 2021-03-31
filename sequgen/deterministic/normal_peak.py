81b658af7afaf9ae91f615f4d835099fd30b6066
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-31 11:46:42 +0200

    removed sign argument from normal_peak signature; refs #60

--- sequgen/deterministic/normal_peak.py
+++ sequgen/deterministic/normal_peak.py
@@ -1,12 +1,11 @@
 import numpy
 
 
-def normal_peak(t_predict, location=0.0, stddev=1.0, height=1.0, sign=1):
+def normal_peak(t_predict, location=0.0, stddev=1.0, height=1.0):
     """ """
 
-    assert sign in [-1, 1], "sign should be -1 or 1"
     max_height = 1 / (stddev * numpy.sqrt(2 * numpy.pi))
     z = (t_predict - location) / stddev
     term1 = 1 / (stddev * numpy.sqrt(2 * numpy.pi))
     term2 = numpy.exp((-1 / 2) * numpy.power(z, 2))
-    return sign * height * term1 * term2 / max_height
+    return height * term1 * term2 / max_height
