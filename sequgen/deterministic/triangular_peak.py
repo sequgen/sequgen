94a3500e584e762e01e901cda54564216b095541
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-03-31 16:54:44 +0200

    simplified triangular peak

--- sequgen/deterministic/triangular_peak.py
+++ sequgen/deterministic/triangular_peak.py
@@ -2,16 +2,12 @@ import numpy
 
 
 # pylint: disable=too-many-arguments
-def triangular_peak(t_predict, width_leading=None, width_base_left=None, width_base_right=None, width_trailing=None,
-                    width=1.0, height=1.0, location=0, sign=1):
+def triangular_peak(t_predict, width_base_left, width_base_right, location, height=1.0):
     """Generate a time series containing a triangular peak.
 
     Args:
       t_predict (Numpy array):
         Where you want the model to generate predictions.
-
-      width_leading (float):
-        The width of the leading part of the triangular peak, i.e. the part before the peak starts to climb.
 
       width_base_left (float):
         The width of the left part of the triangular peak.
@@ -19,53 +15,19 @@ def triangular_peak(t_predict, width_leading=None, width_base_left=None, width_b
       width_base_right (float):
         The width of the right part of the triangular peak.
 
-      width_trailing (float):
-        The width of the trailing part of the triangular peak, i.e. the part after the peak returns back to
-        the baseline.
-
-      width (float):
-        The width of entire peak (excluding any leading and trailing parts).
-
       height (float):
         The height of the peak.
 
       location (float):
         Where the peak should be placed on the time axis.
 
-      sign (float):
-        Whether the peak should be right side up or upside down.
-
     Returns:
       Numpy array of shape equal to t_predict containing the curve for a triangular peak.
     """
 
-    assert sign in [-1, 1], "sign should be -1 or 1"
+    widths = [0, width_base_left, width_base_right]
+    y = [0, height, 0]
 
-    if width is None:
-        assert width_base_left is not None
-        assert width_base_right is not None
+    t = location - width_base_left + numpy.cumsum(widths)
 
-    if width_base_left is None:
-        assert width is not None
-        assert width_base_right is not None
-        width_base_left = width - width_base_right
-
-    if width_base_right is None:
-        assert width is not None
-        assert width_base_left is not None
-        width_base_right = width - width_base_left
-
-    widths = [0, width_leading, width_base_left, width_base_right, width_trailing]
-    y = [0, 0, height, 0, 0]
-
-    if width_leading is None:
-        widths.pop(1)
-        y.pop(1)
-
-    if width_trailing is None:
-        widths.pop(-1)
-        y.pop(-1)
-
-    t = location + numpy.cumsum(widths)
-
-    return sign * numpy.interp(t_predict, t, y)
+    return numpy.interp(t_predict, t, y)
--- tests/deterministic/test_triangular_peak.py
+++ tests/deterministic/test_triangular_peak.py
@@ -1,34 +1,24 @@
 import numpy as np
 from numpy.testing import assert_almost_equal
-
 from sequgen.deterministic.triangular_peak import triangular_peak
 
 
-def test_with_defaults():
-    t_predict = np.arange(7)
-
-    result = triangular_peak(t_predict, width_base_right=2)
-
-    # TODO expected peak=1 at position 0
-    expected = np.array([0.5, 0., 0., 0., 0., 0., 0.])
+def test_with_required_args():
+    t_predict = np.linspace(0, 5, 11)
+    result = triangular_peak(t_predict, width_base_left=1.0, width_base_right=1.0, location=1.0)
+    expected = np.array([0, 0.5, 1.0, 0.5, 0., 0., 0., 0., 0., 0., 0.])
     assert_almost_equal(result, expected)
 
 
-def test_with_placement():
-    t_predict = np.arange(7)
-
-    result = triangular_peak(t_predict, location=3, width_base_right=2)
-
-    # TODO expected peak=1 at position 3
-    expected = np.array([0., 0., 0., 0.5, 0., 0., 0.])
+def test_with_required_args_and_height():
+    t_predict = np.linspace(0, 5, 11)
+    result = triangular_peak(t_predict, width_base_left=1.0, width_base_right=1.0, location=1.0, height=2.0)
+    expected = np.array([0, 1.0, 2.0, 1.0, 0., 0., 0., 0., 0., 0., 0.])
     assert_almost_equal(result, expected)
 
 
-def test_with_base_both_sides():
-    t_predict = np.arange(7)
-
-    result = triangular_peak(t_predict, location=3, width_base_left=2, width_base_right=2)
-
-    # TODO expected peak=1 at position 3
-    expected = np.array([0., 0., 0., 0., 0.5, 1., 0.5])
+def test_with_required_args_and_skewness():
+    t_predict = np.linspace(0, 5, 11)
+    result = triangular_peak(t_predict, width_base_left=1.0, width_base_right=2.0, location=1.0)
+    expected = np.array([0, 0.5, 1.0, 0.75, 0.5, 0.25, 0., 0., 0., 0., 0.])
     assert_almost_equal(result, expected)
