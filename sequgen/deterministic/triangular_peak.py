fcdd01e926a6b57fbb14a246e0c0c9ca6f555e84
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-04-01 11:51:19 +0200

    added asserts for width and added units explanation to docstring

--- sequgen/deterministic/triangular_peak.py
+++ sequgen/deterministic/triangular_peak.py
@@ -10,20 +10,23 @@ def triangular_peak(t_predict, width_base_left, width_base_right, location, heig
         Where you want the model to generate predictions.
 
       width_base_left (float):
-        The width of the left part of the triangular peak.
+        The width of the left part of the triangular peak in units of t_predict.
 
       width_base_right (float):
-        The width of the right part of the triangular peak.
+        The width of the right part of the triangular peak in units of t_predict.
 
       height (float):
-        The height of the peak.
+        The height of the peak in user-defined units.
 
       location (float):
-        Where the peak should be placed on the time axis.
+        Where the peak should be placed on the time axis in units of t_predict.
 
     Returns:
-      Numpy array of shape equal to t_predict containing the curve for a triangular peak.
+      Numpy array of shape equal to t_predict containing the curve for a triangular peak in user-defined units.
     """
+
+    assert width_base_left > 0, "width_base_left should be > 0"
+    assert width_base_right > 0, "width_base_right should be > 0"
 
     widths = [0, width_base_left, width_base_right]
     y = [0, height, 0]
